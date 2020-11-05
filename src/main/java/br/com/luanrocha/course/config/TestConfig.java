f73a4b99e8cbde8bb2e3572601dae0ee7d50884c
Luan Rocha de Souza <luanrocha@outlook.com>
2020-11-04 23:59:18 -0300

    Payment, one-to-one association

--- src/main/java/br/com/luanrocha/course/config/TestConfig.java
+++ src/main/java/br/com/luanrocha/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.luanrocha.course.entities.Category;
 import br.com.luanrocha.course.entities.Order;
 import br.com.luanrocha.course.entities.OrderItem;
+import br.com.luanrocha.course.entities.Payment;
 import br.com.luanrocha.course.entities.Product;
 import br.com.luanrocha.course.entities.User;
 import br.com.luanrocha.course.entities.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
