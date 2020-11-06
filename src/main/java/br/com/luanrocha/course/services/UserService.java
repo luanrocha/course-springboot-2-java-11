c64da88b8bb73c53c2a8c5322d42be75bed14c4d
Luan Rocha de Souza <luanrocha@outlook.com>
2020-11-06 01:06:31 -0300

    User update

--- src/main/java/br/com/luanrocha/course/services/UserService.java
+++ src/main/java/br/com/luanrocha/course/services/UserService.java
@@ -32,4 +32,17 @@ public class UserService {
     public void delete(Long id) {
 	repository.deleteById(id);
     }
+    
+    public User update(Long id, User obj) {
+	//getOne prepara a chamada mas nao se comunica com o db ainda (objeto monitorado jpa)
+	User entity = repository.getOne(id);
+	updateData(entity, obj);
+	return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+	entity.setName(obj.getName());
+	entity.setEmail(obj.getEmail());
+	entity.setPhone(obj.getPhone());
+    }
 }
