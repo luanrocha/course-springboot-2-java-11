9e1500540d5882f95788548f17c4575dcf7d9bfb
Luan Rocha <rochas.luan@gmail.com>
2020-11-07 00:42:10 -0300

    Exception handling - update

--- src/main/java/br/com/luanrocha/course/services/UserService.java
+++ src/main/java/br/com/luanrocha/course/services/UserService.java
@@ -3,6 +3,8 @@ package br.com.luanrocha.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,11 +45,15 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		// getOne prepara a chamada mas nao se comunica com o db ainda (objeto
-		// monitorado jpa)
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		// getOne prepara a chamada mas nao se comunica com o db ainda
+		// (objeto monitorado jpa)
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
